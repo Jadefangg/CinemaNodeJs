6235a1ea27b1f14592da96b0876daa1b4867affa
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-01-26 15:38:27 +0530

    2.2b

--- movie_api/server.js
+++ movie_api/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const url = require('url');
-const fs = require('fs');
+//const fs = require('fs');
 
 http.createServer((request, response) => {
   const path = url.parse(request.url).pathname;
@@ -10,9 +10,23 @@ http.createServer((request, response) => {
 
 console.log('My first Node test server is running on Port 8080.');
 
-fs.readFile('input.txt', (err, data) => {
+/*fs.readFile('input.txt', (err, data) => {
   if (err) {
     throw err;
   }
   console.log('File content: ' + data.toString());
+});*/
+
+const fs = require('fs');
+
+// Get current date and time
+let date = new Date();
+
+// Format date as a string
+let timestamp = date.toString();
+
+// Append timestamp to log.txt
+fs.appendFile('log.txt', timestamp + '\n', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
 });
