44f36fb8c496c046121e02b3c7f8ccf8a146f98a
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-01-26 15:35:40 +0530

    2.2 a
    First update for excercise

--- movie_api/server.js
+++ movie_api/server.js
@@ -1,8 +1,18 @@
 const http = require('http');
+const url = require('url');
+const fs = require('fs');
 
 http.createServer((request, response) => {
+  const path = url.parse(request.url).pathname;
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Hello Node!\n');
 }).listen(8080);
 
 console.log('My first Node test server is running on Port 8080.');
+
+fs.readFile('input.txt', (err, data) => {
+  if (err) {
+    throw err;
+  }
+  console.log('File content: ' + data.toString());
+});
